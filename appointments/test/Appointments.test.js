42705fc51ee6723f9fb5e8d25c6bf61abb371663
Alex Kim <312akim@gmail.com>
2022-02-10 09:20:20 -0800

    Added List Item test

--- appointments/test/Appointments.test.js
+++ appointments/test/Appointments.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import reactDom from 'react-dom';
-import { Appointment } from '../src/Appointment';
+import { Appointment, AppointmentsDayView } from '../src/Appointment';
 
 // 1st arg is Name or description of test. Name same as component being tested.
 // 2nd arg function where define tests
@@ -39,3 +39,38 @@ describe('Appointment', () => {
 // Act: Executes production code under test
 // Assert: Checks expectations are met
 // Short, Descriptive, Independent of other tests, has no side effects
+
+describe('AppontmentsDayView', () => {
+    let container;
+    const today = new Date();
+    const appointments = [
+        {startsAt: today.setHours(12,0)},
+        {startsAt: today.setHours(13,0)},
+    ];
+    
+    beforeEach(() => {
+        container = document.createElement('div');
+    })
+
+    const render = component => {
+        reactDom.render(component, container)
+    }
+    
+    it('renders a div with the right id', () => {
+        render(<AppointmentsDayView appointments={[]} />);
+        expect(container.querySelector('div#appointmentsDayView')).not.toBeNull();
+    })
+
+    it('renders multiple appointments in an ol element', () => {
+        render(<AppointmentsDayView appointments={appointments} />)
+        expect(container.querySelector('ol')).not.toBeNull();
+        expect(container.querySelector('ol').children).toHaveLength(2);
+    })
+
+    it('renders each appointment in an li', () => {
+        render(<AppointmentsDayView appointments={appointments} />);
+        expect(container.querySelectorAll('li')).toHaveLength(2);
+        expect(container.querySelectorAll('li')[0].textContent).toEqual('12:00')
+        expect(container.querySelectorAll('li')[1].textContent).toEqual('13:00')
+    })
+})
